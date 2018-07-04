5cd30a36adb87e8cbb36043730cee6d09fcf5121
jmdressner <jmdressner@gmail.com>
2018-07-04 10:04:18 -0700

    implemented roles from the video tutorial

--- TrashCollector/TrashCollector/App_Start/Startup.Auth.cs
+++ TrashCollector/TrashCollector/App_Start/Startup.Auth.cs
@@ -6,6 +6,7 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
 using TrashCollector.Models;
+using static TrashCollector.ApplicationSignInManager;
 
 namespace TrashCollector
 {
@@ -18,6 +19,7 @@ namespace TrashCollector
             app.CreatePerOwinContext(ApplicationDbContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
             app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
+            app.CreatePerOwinContext<ApplicationRoleManager>(ApplicationRoleManager.Create);
 
             // Enable the application to use a cookie to store information for the signed in user
             // and to use a cookie to temporarily store information about a user logging in with a third party login provider
---
+++ TrashCollector/TrashCollector/Models/RoleViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class RoleViewModel
+    {
+        public RoleViewModel() { }
+
+        public RoleViewModel(ApplicationRole role)
+        {
+            Id = role.Id;
+            Name = role.Name;
+        }
+
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
